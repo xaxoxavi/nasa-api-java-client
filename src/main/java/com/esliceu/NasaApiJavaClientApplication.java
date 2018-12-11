13e2518a1df585b77ab9eaad3e31bc348fb527e1
xaxoxavi <xavi.torrens@gmail.com>
2018-12-11 19:43:04 +0100

    First commit

---
+++ src/main/java/com/esliceu/NasaApiJavaClientApplication.java
@@ -0,0 +1,12 @@
+package com.esliceu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApiJavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasaApiJavaClientApplication.class, args);
+    }
+}
