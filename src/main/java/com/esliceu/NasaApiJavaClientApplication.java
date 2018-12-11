6d3a6bfe0220bb6ecfc022762843a2ed64febb93
xaxoxavi <xavi.torrens@gmail.com>
2018-12-11 20:18:21 +0100

    spring boot rest template example

--- src/main/java/com/esliceu/NasaApiJavaClientApplication.java
+++ src/main/java/com/esliceu/NasaApiJavaClientApplication.java
@@ -1,12 +1,34 @@
 package com.esliceu;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class NasaApiJavaClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NasaApiJavaClientApplication.class, args);
+    private static final Logger log = LoggerFactory.getLogger(NasaApiJavaClientApplication.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(NasaApiJavaClientApplication.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
     }
 }
